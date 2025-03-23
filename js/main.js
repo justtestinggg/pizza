119730c6b8b0a97f2fe020796c0b470fa5535be7
justtestinggg <maxicere@gmail.com>
2025-03-23 17:14:00 -0300

    Agrega override main.js

--- js/main.js
+++ js/main.js
@@ -53,8 +53,8 @@
 	    animateOut: 'fadeOut',
 	    animateIn: 'fadeIn',
 	    nav:false,
-            touchDrag: false,   // Desactiva el arrastre táctil
-            pullDrag: false,    // Desactiva el arrastre al soltar
+            touchDrag: true,   // Desactiva el arrastre táctil
+            pullDrag: true,    // Desactiva el arrastre al soltar
 	    autoplayHoverPause: false,
 	    items: 1,
 	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
